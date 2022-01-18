0307409788933d1f5929b935b203483e0e60ae94
You_Blue <yc424k@gmail.com>
2022-01-18 19:42:06 +0900

    2022년 1월 18일

---
+++ GOORM/[알고리즘히어로즈]셀프레벨테스트1차/1번/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+
+    cin >> N;
+
+    if (N % 400 == 0)
+    {
+        cout << "Leap Year" << endl;
+    }
+    else if (N % 100 == 0)
+    {
+        cout << "Not Leap Year" << endl;
+    }
+    else if (N % 4 == 0)
+    {
+        cout << "Leap Year" << endl;
+    }
+    else
+    {
+        cout << "Not Leap Year" << endl;
+    }
+}
