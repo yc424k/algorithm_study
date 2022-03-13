25c24e02c243b222edb82ebabd65faaad9aa6925
yc424k <yc424k@gmail.com>
2022-03-13 23:05:47 +0900

    임시

---
+++ BAEKJOON/Bronze4/10101/main.py
@@ -0,0 +1,4 @@
+arr = list(map(int,input().split()))
+arr.sort()
+if arr[1] == 60:
+    print("Equilateral")
--- main.py
+++ main.py
@@ -1,15 +1,1 @@
-#5543
-import math
-list1 = []
-list2 = []
-for i in range(3):
-    list1.append(input())
-    
-for i in range(2):
-    list2.append(input())
-
-
-list1.sort()
-list2.sort()
-d=int(list1[0])+int(list2[0])-50
-print(d)
+print(int(input()+1))
