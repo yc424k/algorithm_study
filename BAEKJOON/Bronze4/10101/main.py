3c20070fbd9c02b14c1f1320d155efaf27c154ac
yc424k <yc424k@gmail.com>
2022-03-15 22:55:52 +0900

    2022년 3월 15일

--- BAEKJOON/Bronze4/10101/main.py
+++ BAEKJOON/Bronze4/10101/main.py
@@ -1,4 +1,10 @@
-arr = list(map(int,input().split()))
+arr = list(map(int,open(0)))
 arr.sort()
-if arr[1] == 60:
+if sum(arr) != 180:
+    print("Error")
+elif arr[1] == 60 and arr[2] == 60:
     print("Equilateral")
+elif arr[0] == arr[1] or arr[1] == arr[2]:
+    print("Isosceles")
+else:
+    print("Scalene")
